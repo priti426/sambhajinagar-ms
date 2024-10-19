81b27a420985824cb603babd5373785c7d2a8b5f
priti426 <pritishelke7777@gmail.com>
2024-10-19 11:24:30 +0530

    Hello

---
+++ src/main/java/com/example/sambhajinagar_ms/SambhajinagarMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sambhajinagar_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SambhajinagarMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SambhajinagarMsApplication.class, args);
+	}
+
+}
