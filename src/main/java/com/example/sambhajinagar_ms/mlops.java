c0eb040abf950fbfc4bee4d5b5b6eeec0213a2e0
priti426 <pritishelke7777@gmail.com>
2024-10-19 14:17:01 +0530

    adding java class

---
+++ src/main/java/com/example/sambhajinagar_ms/mlops.java
@@ -0,0 +1,9 @@
+package com.example.sambhajinagar_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class mlops {
+    @GetMapping("/mlops")
+    public String getData() {return  "Welcome to mlops Class" ; }
+}
