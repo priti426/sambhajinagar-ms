a5500c143e832b6c6f7cf49765c31141c5c3b17e
priti426 <pritishelke7777@gmail.com>
2024-10-19 13:09:18 +0530

    adding java class

--- src/main/java/com/example/sambhajinagar_ms/ccna.java
+++ src/main/java/com/example/sambhajinagar_ms/ccna.java
@@ -2,9 +2,8 @@ package com.example.sambhajinagar_ms;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
-public class park {
-    @GetMapping("/park")
-    public String getData() {return  "Welcome to Devops Class" ; }
+public class ccna {
+    @GetMapping("/ccna")
+    public String getData() {return  "Welcome to ccna Class" ; }
 }
