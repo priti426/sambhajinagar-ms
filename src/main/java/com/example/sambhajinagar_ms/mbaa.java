c553be7586630d59df171a24beb03d29be430f69
priti426 <pritishelke7777@gmail.com>
2024-11-06 15:49:59 +0530

    adding java class

--- src/main/java/com/example/sambhajinagar_ms/mbaa.java
+++ src/main/java/com/example/sambhajinagar_ms/mbaa.java
@@ -3,7 +3,8 @@ package com.example.sambhajinagar_ms;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-public class ccna {
-    @GetMapping("/ccna")
-    public String getData() {return  "Welcome to ccna learn aws stagemaker kubeflow mlflow tensorflow Class" ; }
+public class mbaa {
+    @GetMapping("/mbaa")
+    public String getData() {return  "Welcome to mbaa class" ; }
 }
+
